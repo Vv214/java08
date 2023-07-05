62567340d90a50c31b0a95d032f559b7f2d75c1c
Vv214 <vrf.lbc@gmail.com>
2023-07-05 09:26:07 +0200

    Tp done

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -17,8 +17,43 @@ public class Eagle extends Bird {
         return flying;
     }
 
+    public void setAltitude(int altitude) {
+        this.altitude = this.altitude + altitude;
+    }
+
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    @Override
+    public void takeOff() {
+        System.out.printf("%s takes off in the sky.\n", this.getName());
+    }
+
+    @Override
+    public void ascend(int altitude) {
+        setAltitude(altitude);
+        System.out.printf("%s flies upward, altitude: %d.\n", this.getName(), this.altitude);
+    }
+
+    @Override
+    public void glide() {
+        System.out.printf("glides into the air. \n");
+    }
+
+    @Override
+    public void descend(int altitude) {
+        setAltitude(-altitude);
+        System.out.printf("%s flies downward, altitude: %d.\n", this.getName(), this.altitude);
+    }
+
+    @Override
+    public void land() {
+        if (this.altitude > 9) {
+            System.out.printf("%s is too high, it can't land. \n", this.getName());
+        } else {
+            System.out.printf("%s lands on the ground.\n", this.getName());
+        }
     }
 }
